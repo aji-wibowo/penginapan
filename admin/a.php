c8922bff1f3f6412139f89e126a3281b3197ce53
Pratama Ardy Prayoga <maxtkj@outlook.com>
2020-04-12 16:13:33 -0700

    Layouting dan tambah menu

---
+++ admin/a.php
@@ -0,0 +1,9 @@
+<?php
+$page = "Test";
+session_start();
+require 'layout/header_dashboard.php';
+?>
+<h1>TEST</h1>
+<?php
+require 'layout/footer_dashboard.php';
+?>
--- admin/dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-$page = "Dashboard Admin";
-session_start();
-require 'layout/header_dashboard.php';
-require 'lib/session_admin.php';
-?>
-<h1>Ini halaman admin</h1>
-<a href="<?=base_url()?>admin/logout.php">Keluar</a>
