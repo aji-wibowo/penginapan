471845773e34a3c1b154bb41034ca7653e37f6a0
Pratama Ardy Prayoga <maxtkj@outlook.com>
2020-04-14 15:01:01 -0700

    CRUD Kelola Kamar

--- admin/lib/room_management/list.php
+++ admin/lib/room_management/list.php
@@ -19,7 +19,36 @@ if (isset($_POST['add_data'])) {
 		if ($connect->query("INSERT INTO kamar VALUES ('','$room_name','$room_type','$room_address','$room_price','$kd_lokasi')") == true) {
 			$_SESSION['notification'] = array('alert' => 'success', 'title' => 'Sukses', 'message' => 'Data berhasil ditambahkan.');
 		}else{
-			$_SESSION['notification'] = array('alert' => 'danger', 'title' => 'Gagal', 'message' => 'Fatal error!'.$kd_lokasi);
+			$_SESSION['notification'] = array('alert' => 'danger', 'title' => 'Gagal', 'message' => 'Fatal error!'.mysqli_error($connect));
+		}
+	}
+}
+if (isset($_POST['edit_data'])) {
+	$room_name = $connect->real_escape_string(filter($_POST['room_name']));
+	$room_type = $connect->real_escape_string(filter($_POST['room_type']));
+	$room_address = $connect->real_escape_string(filter($_POST['room_address']));
+	$room_price = $connect->real_escape_string(filter($_POST['room_price']));
+	$kd_kamar = $connect->real_escape_string(filter($_POST['kd_kamar']));
+
+	if (!$room_name || !$room_type || !$room_address|| !$room_price|| !$kd_kamar) {
+		$_SESSION['notification'] = array('alert' => 'danger', 'title' => 'Gagal', 'message' => 'Harap mengisi semua form.');
+	}else{
+		if ($connect->query("UPDATE kamar SET nama_kamar = '$room_name', tipe_kamar = '$room_type', alamat_kamar = '$room_address', harga_kamar = '$room_price' WHERE kd_kamar = '$kd_kamar'") == true) {
+			$_SESSION['notification'] = array('alert' => 'success', 'title' => 'Sukses', 'message' => 'Data berhasil diubah.');
+		}else{
+			$_SESSION['notification'] = array('alert' => 'danger', 'title' => 'Gagal', 'message' => 'Fatal error!'.mysqli_error($connect));
+		}
+	}
+}
+if (isset($_POST['delete_data'])) {
+	$kd_kamar = $connect->real_escape_string(filter($_POST['kd_kamar']));
+	if (!$kd_kamar) {
+		$_SESSION['notification'] = array('alert' => 'danger', 'title' => 'Gagal', 'message' => 'Harap mengisi semua form.');
+	}else{
+		if ($connect->query("DELETE FROM kamar WHERE kd_kamar = '$kd_kamar'") == true) {
+			$_SESSION['notification'] = array('alert' => 'success', 'title' => 'Sukses', 'message' => 'Data berhasil dihapus.');
+		}else{
+			$_SESSION['notification'] = array('alert' => 'danger', 'title' => 'Gagal', 'message' => 'Fatal error!'.mysqli_error($connect));
 		}
 	}
 }
@@ -131,6 +160,7 @@ if (isset($_POST['add_data'])) {
 								<thead>
 									<tr>
 										<th>No.</th>
+										<th>Nama Kamar</th>
 										<th>Tipe Kamar</th>
 										<th>Harga Kamar</th>
 										<th>Kode Lokasi</th>
@@ -144,20 +174,106 @@ if (isset($_POST['add_data'])) {
 									while ($data_rooms = $check_rooms->fetch_assoc()) {
 										?>  
 										<tr>
-											<td><?=$no++?></td>
+											<td><?=$no?></td>
+											<td><?=$data_rooms['nama_kamar']?></td>
 											<td><?=$data_rooms['tipe_kamar']?></td>
 											<td><?=$data_rooms['harga_kamar']?></td>
-											<td><?=$data_rooms['kd_kamar']." / ".$data_rooms['kota']?></td>
+											<td><?=$data_rooms['kd_lokasi']." / ".$data_rooms['kota']?></td>
 											<td>
-												<button type="button" class="btn btn-warning btn-xs"><i class="fas fa-pen"></i></button>
-												<button type="button" class="btn btn-danger btn-xs"><i class="fas fa-trash"></i></button>
+												<button type="button" class="btn btn-warning btn-xs" data-toggle="modal" data-target="#modal-edit-data<?=$no?>"><i class="fas fa-pen"></i></button>
+												<div class="modal fade" id="modal-edit-data<?=$no?>">
+													<div class="modal-dialog">
+														<div class="modal-content">
+															<div class="modal-header">
+																<h4 class="modal-title">Ubah Data Kamar #<?=$no?></h4>
+																<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																	<span aria-hidden="true">&times;</span>
+																</button>
+															</div>
+															<div class="modal-body">
+																<form method="POST">
+																	<div class="input-group mb-3">
+																		<input type="text" name="room_name" class="form-control" placeholder="Nama Kamar" value="<?=$data_rooms['nama_kamar']?>">
+																		<div class="input-group-append">
+																			<div class="input-group-text">
+																				<span class="fa fa-door-open"></span>
+																			</div>
+																		</div>
+																	</div>
+																	<div class="input-group mb-3">
+																		<input type="text" name="room_type" class="form-control" placeholder="Tipe Kamar" value="<?=$data_rooms['tipe_kamar']?>">
+																		<div class="input-group-append">
+																			<div class="input-group-text">
+																				<span class="fa fa-bed"></span>
+																			</div>
+																		</div>
+																	</div>
+																	<div class="input-group mb-3">
+																		<textarea name="room_address" class="form-control" rows="2" placeholder="Alamat Kamar ..."><?=$data_rooms['alamat_kamar']?></textarea>
+																		<div class="input-group-append">
+																			<div class="input-group-text">
+																				<span class="fas fa-map-marker-alt"></span>
+																			</div>
+																		</div>
+																	</div>
+																	<div class="input-group mb-3">
+																		<input type="number" name="room_price" class="form-control" placeholder="Harga Kamar" value="<?=$data_rooms['harga_kamar']?>">
+																		<div class="input-group-append">
+																			<div class="input-group-text">
+																				<span class="fa fa-money-bill-wave"></span>
+																			</div>
+																		</div>
+																	</div>
+																	<input type="text" name="kd_kamar" value="<?=$data_rooms['kd_kamar']?>" hidden>
+																	<p>*<small>Pada <i>harga kamar</i> input nomor saja tanpa tanda baca.</small></p>
+																</div>
+																<div class="modal-footer">
+																	<button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-times btn-xs"></i> Batal</button>
+																	<button type="submit" name="edit_data" class="btn btn-success"><i class="fas fa-paper-plane btn-xs"></i> Simpan</button>
+																</form>
+															</div>
+														</div>
+														<!-- /.modal-content -->
+													</div>
+													<!-- /.modal-dialog -->
+												</div>
+												<!-- /.modal -->
+												<button type="button" class="btn btn-danger btn-xs" data-toggle="modal" data-target="#modal-delete-data<?=$no?>"><i class="fas fa-trash"></i></button>
+												<div class="modal fade" id="modal-delete-data<?=$no?>">
+													<div class="modal-dialog">
+														<div class="modal-content">
+															<div class="modal-header">
+																<h4 class="modal-title">Hapus Data Kamar #<?=$no?></h4>
+																<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+																	<span aria-hidden="true">&times;</span>
+																</button>
+															</div>
+															<div class="modal-body">
+																<form method="POST">
+																	<p>Apakah anda yakin akan menghapus data kamar no.<?=$no?>?</p>
+																	<input type="text" name="kd_kamar" value="<?=$data_rooms['kd_kamar']?>" hidden>
+																</div>
+																<div class="modal-footer">
+																	<button type="submit" name="delete_data" class="btn btn-danger"><i class="fas fa-paper-plane btn-xs"></i> Yakin, Hapus</button>
+																	<button type="button" class="btn btn-success" data-dismiss="modal"><i class="fas fa-times btn-xs"></i> Batal</button>
+																</form>
+															</div>
+														</div>
+														<!-- /.modal-content -->
+													</div>
+													<!-- /.modal-dialog -->
+												</div>
+												<!-- /.modal -->
 											</td>
 										</tr>
-									<?php } ?>        
+										<?php 
+										$no++;
+									} ?>        
 								</tbody>
 								<tfoot>
 									<tr>
 										<th>No.</th>
+										<th>Nama Kamar</th>
 										<th>Tipe Kamar</th>
 										<th>Harga Kamar</th>
 										<th>Kode Lokasi</th>
