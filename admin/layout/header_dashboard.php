f55bd64a77897c781acbd831aa81b1cfad345a06
Pratama Ardy Prayoga <maxtkj@outlook.com>
2020-04-16 23:54:38 -0700

    Admin header status login

--- admin/layout/header_dashboard.php
+++ admin/layout/header_dashboard.php
@@ -46,6 +46,9 @@
 				<li class="nav-item">
 					<a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
 				</li>
+				<li class="nav-item d-none d-sm-inline-block">
+					<a class="nav-link">Anda login sebagai <b><?=$_SESSION['admin']['username']?></b> dengan role Admin</a>
+				</li>
 			</ul>
 
 			<!-- Right navbar links -->
