c1cfcca34c3d61c3600688af6c522a5e0682b70c
Aji Wibowo <aji.wibowo@indocyber.co.id>
2020-04-18 09:44:57 +0700

    dashboard admin

--- admin/layout/footer_dashboard.php
+++ admin/layout/footer_dashboard.php
@@ -79,6 +79,62 @@
       "responsive": true,
     });
   });
+
+  $(document).ready(function(){
+    var arrayLabel = $.parseJSON('<?= js_array($months) ?>');
+    var dataChart = $.parseJSON('<?= js_array($dataChart) ?>');
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: arrayLabel,
+        datasets: [{
+          label: 'reservation',
+          data: dataChart,
+          backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+          ],
+          borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        },
+        responsive: true
+      }
+    });
+  })
+
 </script>
 </body>
 </html>
